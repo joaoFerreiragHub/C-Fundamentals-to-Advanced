97d496afc134be4a7d21a38e6c7dc6696a5e6b50
joaoFerreiragHub <96747056+joaoFerreiragHub@users.noreply.github.com>
2022-04-17 17:13:05 +0100

    update

---
+++ C# ASP.net CORE/FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+        public IActionResult Index()
+        {
+            
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            //return View("Index");
+            dogs.Add(dogViewModel);
+            return RedirectToAction("Index", nameof(Index));
+        }
+
+        public IActionResult Hello()
+        {
+            DogViewModel doggo = new DogViewModel() { Name = "Sif", Age = 2 };
+            return View(doggo);
+        }
+    }
+}
