6639fd6c93814e211c57a8959ec2f3e2d096fa76
joaoFerreiragHub <96747056+joaoFerreiragHub@users.noreply.github.com>
2022-04-21 13:41:20 +0100

    morePractice

---
+++ C# ASP.net CORE/Blazor/File_Upload/File_Upload/Services/FileDownload.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.JSInterop;
+
+namespace File_Upload.Services
+{
+    public interface IFileDownload
+    {
+        Task<List<string>> GetUploadedFiles();
+        Task DownloadFile(string url);
+    }
+    public class FileDownload : IFileDownload
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+        private readonly IJSRuntime _js;
+
+        public FileDownload(IWebHostEnvironment webHostEnvironment, IJSRuntime js)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _js = js;
+        }
+        public async Task DownloadFile(string url)
+        {
+            await _js.InvokeVoidAsync("downloadFile", url);
+        }
+
+        public async Task<List<string>> GetUploadedFiles()
+        {
+            var base64Urls = new List<string>();
+            var uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "uploads");
+            var files = Directory.GetFiles(uploadPath);
+
+            if (files is not null && files.Length > 0)
+            {
+                foreach (var file in files)
+                {
+                    using (var fileInput =
+                        new FileStream(file,FileMode.Open, FileAccess.Read))
+                    {
+                        var memorystream = new MemoryStream();
+                        await fileInput.CopyToAsync(memorystream);
+
+                        var buffer = memorystream.ToArray();
+                        var fileType = GetMineTypeForFileExtension(file);
+                        base64Urls.Add($"data:{ fileType}; base64,{ Convert.ToBase64String(buffer) }");
+                    }                       
+                }
+            }
+            return base64Urls;
+        }
+
+        private string GetMineTypeForFileExtension(string filePath)
+        {
+            const string DefaultContentType = "application/octet-stream";
+            var provider = new FileExtensionContentTypeProvider();
+
+            if (!provider.TryGetContentType(filePath, out string contetType))
+            {
+                contetType = DefaultContentType;
+            }
+            return contetType;
+        }
+    }
+}
---
+++ C# ASP.net CORE/Blazor/firstBlazorApp/firstBlazorApp/Data/Grocery.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace firstBlazorApp.Data    
+{
+    public class Grocery
+    {
+        [Required]
+        [StringLength(15, ErrorMessage ="Name should be less than 15 characters.")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1,1000, ErrorMessage = "Valid Price Range is (1-10000")]
+        public float Price { get; set; }
+
+    }
+}
