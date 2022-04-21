3c30c1c9cc0299958ac847d4583283554fa752b6
joaoFerreiragHub <96747056+joaoFerreiragHub@users.noreply.github.com>
2022-04-21 14:56:00 +0100

    LAST

---
+++ C# ASP.net CORE/MVCLibrary/MVCLibrary/Controllers/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCLibrary.Controllers
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string CallNumber { get; set; }
+
+        public string Author { get; set; }
+    }
+}
---
+++ C# ASP.net CORE/MVCLibrary/MVCLibrary/Data/LibraryContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCLibrary.Controllers;
+
+namespace MVCLibrary.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext (DbContextOptions<LibraryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCLibrary.Controllers.Book> Book { get; set; }
+    }
+}
