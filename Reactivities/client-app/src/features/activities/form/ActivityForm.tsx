91d8aa1affd298f2a6ec8f119200a2b046fd89a5
joaoFerreiragHub <96747056+joaoFerreiragHub@users.noreply.github.com>
2022-10-08 22:21:13 +0100

    update

---
+++ Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,115 @@
+import { observer } from "mobx-react-lite";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { Link, useHistory, useParams } from "react-router-dom";
+import { Button, FormField, Label, Segment } from "semantic-ui-react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { useStore } from "../../../app/stores/store";
+import { v4 as uuid } from "uuid";
+import { Formik, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import MyTextInput from "../../../app/common/form/MyTextInput";
+import MyTextArea from "../../../app/common/form/MyTextArea";
+import MySelectInput from "../../../app/common/form/MySelectInput";
+import { categoryOptions } from "../../../app/common/options/categoryOptions";
+import MyDateInput from "../../../app/common/form/MyDateInput";
+
+export default observer(function ActivityForm() {
+  const history = useHistory();
+  const { activityStore } = useStore();
+  const {
+    createActivity,
+    updateActivity,
+    loading,
+    loadActivity,
+    loadingInitial,
+  } = activityStore;
+  const { id } = useParams<{ id: string }>();
+
+  const [activity, setActivity] = useState({
+    id: "",
+    title: "",
+    category: "",
+    description: "",
+    date: "",
+    city: "",
+    venue: "",
+  });
+
+  const validationSchema = Yup.object({
+    title: Yup.string().required(`The Activity title is required`),
+    description: Yup.string().required(`The Activity description is required`),
+    category: Yup.string().required(),
+    date: Yup.string().required(),
+    venue: Yup.string().required(),
+    city: Yup.string().required(),
+  });
+
+  useEffect(() => {
+    if (id) loadActivity(id).then((activity) => setActivity(activity!));
+  }, [id, loadActivity]);
+
+  // function handleSubmit() {
+  //     if (activity.id.length === 0) {
+  //         let newActivity = {
+  //             ...activity,
+  //             id: uuid()
+  //         };
+  //         createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`))
+  //     } else {
+  //         updateActivity(activity).then(() => history.push(`/activities/${activity.id}`))
+  //     }
+  // }
+
+  // function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+  //     const {name, value} = event.target;
+  //     setActivity({...activity, [name]: value})
+  // }
+
+  if (loadingInitial) return <LoadingComponent content="Loading activity..." />;
+
+  return (
+    <Segment clearing>
+      <Formik
+        validationSchema={validationSchema}
+        enableReinitialize
+        initialValues={activity}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ values: activity, handleChange, handleSubmit }) => (
+          <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
+            <MyTextInput name="title" placeholder="Title" />
+            <MyTextArea rows={3} placeholder="Description" name="description" />
+            <MySelectInput
+              options={categoryOptions}
+              placeholder="Category"
+              name="category"
+            />
+            <MyDateInput
+              placeholder="Date"
+              name="date"
+              showTimeSelect
+              timCaption="time"
+              dateFormat="MMMM d, yyyy h:m aa"
+            />
+            <MyTextInput placeholder="City" name="city" />
+            <MyTextInput placeholder="Venue" name="venue" />
+            <Button
+              loading={loading}
+              floated="right"
+              positive
+              type="submit"
+              content="Submit"
+            />
+            <Button
+              as={Link}
+              to="/activities"
+              floated="right"
+              type="button"
+              content="Cancel"
+            />
+          </Form>
+        )}
+      </Formik>
+    </Segment>
+  );
+});
